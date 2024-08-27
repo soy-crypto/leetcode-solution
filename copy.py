6c3399dc60710831cbdb46e9de7e9f8241f3f24e
tengjiao zhu <ztjnwu@gmail.com>
2024-08-27 22:46:25 +0000

    08-27

--- copy.py
+++ copy.py
@@ -12,26 +12,46 @@ for folder in os.listdir(directory):
     print("!folder " + folder)
     # Open file
     newfolder = "./problems/" + folder
-    print("!newfolder " + newfolder)
-    for name in os.listdir(newfolder):
-        # print("name " + name)
-        filename = os.path.join(folder, "README.md")
-        # print("filename " + filename)
-        if os.path.exists(filename) is True:
-            with open(filename) as f:
-                #print(f"Content of '{name}'")
-                # Read content of file
-                #print(f.read())
-                newfilename = destination + folder + '.html'
-                shutil.copyfile(filename, newfilename)
-                #print("True")
-                count = count + 1
-                    
-        else:
-            
+    #print("!newfolder " + newfolder)
+    #for name in os.listdir(newfolder):
+    #print("name " + name)
+    filename = os.path.join(newfolder, "README.md")
+    print("filename " + filename)
+    if os.path.exists(filename) is True:
+        with open(filename) as f:
+            #print(f"Content of '{name}'")
+            # Read content of file
+            #print(f.read())
             newfilename = destination + folder + '.html'
-            with open(newfilename, 'w') as fp: 
-                pass
-            list.append(newfilename)
+            shutil.copyfile(filename, newfilename)
+            #print("True")
+        count = count + 1
+                
+    else:
+        
+        newfilename = destination + folder + '.html'
+        with open(newfilename, 'w') as fp: 
+            pass
+        list.append(newfilename)
     
-    print("count ", count)
+print("Count: ", count)
+
+
+'''
+#convert html to pdf
+from pyhtml2pdf import converter
+
+for folder in os.listdir(directory):
+    print("!folder " + folder)
+    # Open file
+    newfolder = "./problems/" + folder
+    #print("!newfolder " + newfolder)
+    #for name in os.listdir(newfolder):
+    #print("name " + name)
+    filename = os.path.join(newfolder, "README.md")
+    print("filename " + filename)
+    newfilename = destination + folder + '.html'
+    newfilenamePdf =  destination + folder + '.pdf'
+    path = os.path.abspath(newfilename)
+    converter.convert(f'file:///{path}', f'file:///{newfilenamePdf}')
+'''
