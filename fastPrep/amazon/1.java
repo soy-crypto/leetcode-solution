d0214a9e7d09cb0f65bacc82305af0ac4eef4c43
tengjiao zhu <ztjnwu@gmail.com>
2024-08-30 18:48:55 +0000

    08-30

--- fastPrep/amazon/1.java
+++ fastPrep/amazon/1.java
@@ -1,8 +1,10 @@
+/* 
 Minimize Sum of Distances to Warehouses
 🔥 FULLTIME
 📚
 RELATED PROBLEMS
-Amazon has recently established n distribution centers in a new location. They want to set up 2 warehouses to serve these distribution centers. Note that the centers and warehouses are all built along a straight line. A distribution center has its demands met by the warehouse that is closest to it. A logistics team wants to choose the location of the warehouses such that the sum of the distances of the distribution centers to their closest warehouses is minimized.
+Amazon has recently established n distribution centers in a new location. 
+They want to set up 2 warehouses to serve these distribution centers. Note that the centers and warehouses are all built along a straight line. A distribution center has its demands met by the warehouse that is closest to it. A logistics team wants to choose the location of the warehouses such that the sum of the distances of the distribution centers to their closest warehouses is minimized.
 
 Given an array dist_centers, that represent the positions of the distribution centers, return the minimum sum of distances to their closest warehouses if the warehouses are positioned optimally.
 
@@ -22,13 +24,8 @@ Example 1:
 Input:  dist_centers = [1, 6]
 Output: 0 
 Explanation:
-
+Optimal solution: Place one warehouse at x1 = 1 and the other at x2 = 6. The sum of distances to the nearest warehouse is 0 + 0 = 0.
       
-
-      Optimal solution: Place one warehouse at x1 = 1 and the other at x2 = 6. The sum of distances to the nearest warehouse is 0 + 0 = 0.
-      
-
-
       
 Example 2:
 
@@ -40,6 +37,8 @@ Explanation:
 
       Optimal solution: Place one warehouse at x1 = 1 and the other at x2 = 6. The distances to the nearest warehouse are [0, 0, 1, 0], resulting in a total distance sum of 2.
       
+
+*/
 
 
 //solution
---
+++ fastPrep/amazon/2.java
@@ -0,0 +1,118 @@
+/* 
+Reduce Gifts
+🔥 FULLTIME
+📚
+RELATED PROBLEMS
+New Year's Day is around the corner and Amazon is having a sale. They have a list of items they are considering but they may need to remove some of them. Determine the minimum number of items to remove from an array of prices so that the sum of prices of any k items does not exceed a threshold.
+
+Note: If the number of items in the list is less than k, then there is no need to remove any more items.
+
+Function Description
+
+Complete the function reduceGifts in the editor.
+
+reduceGifts has the following parameters:
+
+1. int prices[n]: the prices of each item
+2. int k: the number of items to sum
+3. int threshold: the maximum price of k items
+Returns
+
+int: the minimum number of items to remove
+
+🌷ଓ༉｡ﾟCredit to ꒰ა buddi ໒꒱
+
+Example 1:
+
+Input:  prices = [3, 2, 1, 4, 6, 5], k = 3, threshold = 14
+Output: 1 
+Explanation:
+
+
+
+The sum of prices for every k = 3 items must not be more than threshold = 14. The sum of the prices of the last three items is 6 + 5 + 4 = 15. The item priced $6 can be removed leaving:
+  
+
+
+No 3 items' prices sum to greater than 14. Only 1 item needs to be removed.
+      
+Example 2:
+
+Input:  prices = [9, 6, 7, 2, 7, 2], k = 2, threshold = 13
+Output: 2 
+Explanation:
+
+Items with prices 9 and 7 have a sum larger than 13. After removing these two items, prices' = [6, 2, 7, 2].
+
+No k items have a sum of prices greater than threshold.
+      
+Example 3:
+
+Input:  prices = [9, 6, 3, 2, 9, 10, 10, 11], k = 4, threshold = 1
+Output: 5 
+Explanation:
+
+Since no price is less than or equal to threshold, the sum of any k elements will always be greater than threshold.
+
+The threshold only applies to goups of k items. Now the goal is to reduce the arr length to k - 1 which requires the removal of 5 elements.
+      
+Constraints:
+1 <= k <= n <= 105
+1 <= threshold <= 109
+1 <= prices[i] <= 100
+
+
+*/
+
+public int reduceGifts(int[] prices, int k, int threshold) 
+{
+  //Base Check
+  if(prices == null || prices.length < k)
+  {
+    return 0;
+  }
+
+  //Init
+  int value;
+
+  //Compute
+  /** Sort the given array */
+  Arrays.sort(prices);
+
+  /** Find the minimum number of elements to be removed */
+  int prevSum = 0;
+  for(int i = prices.length - 1; i >= prices.length - (k - 1); i--)
+  {
+    prevSum += prices[i];
+  }
+
+  int index = k - 2;
+  for(int i = prices.length - 1; i >= k - 1; i --)
+  {
+    //current subaray of size k
+    /** compute the sum of elements in the current subarray */
+    int currentSum = prevSum;
+    currentSum -= i + 1 == prices.length ? 0 : prices[i + 1];
+    currentSum += prices[i - (k - 1)];
+
+    /** check if the current subarray satisfies the requirement */
+    if(currentSum <= threshold)
+    {
+      index = i;
+      break;
+    }
+
+    //update prevSum
+    prevSum = currentSum;
+    
+  }
+
+  /** update value */
+  value = (prices.length - 1) - index;
+
+  //Return
+  return value;
+  
+}
+
+
